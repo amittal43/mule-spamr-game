7eeab4934028cc44437f036a7a1c982310dfa105
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-07 23:36:24 +0000

    added resources under player data with getters and setters

--- mule - spamr/src/edu/gatech/spamr/model/Player.java
+++ mule - spamr/src/edu/gatech/spamr/model/Player.java
@@ -2,14 +2,25 @@ package edu.gatech.spamr.model;
 
 public class Player {
 
+	//data
 	private String name;
 	private Race race;
 	private Difficulty diff;
 	private Color color;
+	private int money;
+	private int food;
+	private int energy;
+	private int ore;
+	
+	//array of Tile's which the player owns
+	
+	//enums
 	public enum Race{PACKER, SPHEROID, HUMANOID, LEGGITE, FLAPPER, BONZOID, MECHTRON, GOLLUMER};
 	public enum Difficulty{BEGINNER, STANDARD, TOURNAMENT};
 	public enum Color{BLUE, GREEN, RED, YELLOW};
 	
+	
+	//getters and setters
 	public String getName(){
 		return name;
 	}
@@ -41,4 +52,41 @@ public class Player {
 	public void setColor(Color c){
 		color = c;
 	}
+	
+	public int getMoney(){
+		return money;
+	}
+	
+	public void setMoney(int m){
+		money = m;
+	}
+	
+	public int getFood(){
+		return food;
+	}
+	
+	public void setFood(int f){
+		food = f;
+	}
+	
+	public int getEnergy(){
+		return energy;
+	}
+	
+	public void setEnergy(int e){
+		energy = e;
+	}
+	
+	public int getOre(){
+		return ore;
+	}
+	
+	public void setOre(int o){
+		ore = o;
+	}
+	
 }
+
+
+
+
