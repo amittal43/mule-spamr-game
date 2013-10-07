5241cf6a3a48bfc56d8fb542e9e78a5f25b17a0b
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-07 22:31:45 +0000

    added race attribute to player

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Player.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Player.java
@@ -3,6 +3,7 @@ package edu.gatech.spamr.model;
 public class Player {
 
 	private String name;
+	private String race;
 	private enum race{PACKER, SPHEROID, HUMANOID, LEGGITE, FLAPPER, BONZOID, MECHTRON, GOLLUMER};
 	public enum difficulty{BEGINNER, STANDARD, TOURNAMENT};
 	private enum color{BLUE, GREEN, RED, YELLOW};
@@ -14,4 +15,13 @@ public class Player {
 	public void setName(String n){
 		name = n;
 	}
+	
+	public String getRace(){
+		return race;
+	}
+	
+	public void setRace(String n){
+		race = n;
+	}
+	
 }
