16e08e8467a8985c8234a5048bb078cecd0c7799
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-07 22:27:24 +0000

    Started Player and PlayerConfig UI classes. Added resources package including images.

---
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Player.java
@@ -0,0 +1,17 @@
+package edu.gatech.spamr.model;
+
+public class Player {
+
+	private String name;
+	private enum race{PACKER, SPHEROID, HUMANOID, LEGGITE, FLAPPER, BONZOID, MECHTRON, GOLLUMER};
+	public enum difficulty{BEGINNER, STANDARD, TOURNAMENT};
+	private enum color{BLUE, GREEN, RED, YELLOW};
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+}
