2e4914f03c0bd51409f1064b20c28e5866f78416
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-08 00:07:43 +0000

    added Map choice to GameConfigUI

--- mule - spamr/src/edu/gatech/spamr/view/GameConfigUI.java
+++ mule - spamr/src/edu/gatech/spamr/view/GameConfigUI.java
@@ -1,6 +1,10 @@
 package edu.gatech.spamr.view;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.DefaultComboBoxModel;
 
 public class GameConfigUI extends JPanel {
 
@@ -8,6 +12,14 @@ public class GameConfigUI extends JPanel {
 	 * Create the panel.
 	 */
 	public GameConfigUI() {
+		setBackground(Color.DARK_GRAY);
+		setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Default Map", "Other Map"}));
+		comboBox.setFont(new Font("Verdana", Font.BOLD, 11));
+		comboBox.setBounds(73, 153, 103, 20);
+		add(comboBox);
 
 	}
 
