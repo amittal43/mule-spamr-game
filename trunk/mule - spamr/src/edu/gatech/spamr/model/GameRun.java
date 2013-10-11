73c2f680ffc1a3e4d8096711cd427ae529c49d47
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-11 16:43:48 +0000

    added import to view package

--- trunk/mule - spamr/src/edu/gatech/spamr/model/GameRun.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/GameRun.java
@@ -1,10 +1,13 @@
 package edu.gatech.spamr.model;
 
+import edu.gatech.spamr.view.*;
+
 public class GameRun {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		GameConfigUI gUI = new GameConfigUI();
 		
 		
 	}
