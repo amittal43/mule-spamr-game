4f15b473b68e8272d5d740479d3d2e9631470087
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-11 17:55:38 +0000

    Added ActionListeners to SelectPlayer RadioButtons and started Adding ActionListeners to Race buttons

--- trunk/mule - spamr/src/edu/gatech/spamr/view/PlayerConfigUI.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/view/PlayerConfigUI.java
@@ -3,30 +3,46 @@ package edu.gatech.spamr.view;
 import javax.swing.JPanel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+
+import edu.gatech.spamr.model.Player;
 import edu.gatech.spamr.model.Player.Difficulty;
+import edu.gatech.spamr.model.Player.Race;
+
 import javax.swing.JTable;
+
 import java.awt.Component;
+
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Cursor;
 import java.awt.Color;
+
 import javax.swing.JToggleButton;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+
 import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class PlayerConfigUI extends JPanel {
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private final ButtonGroup buttonGroup_1 = new ButtonGroup();
+	private final ButtonGroup SelectPlayer = new ButtonGroup();
+	private final ButtonGroup SelectRace = new ButtonGroup();
 	private JTextField textField;
+	private Player player1, player2, player3, player4;
+	private Player selectedPlayer = null;
+	
+	private Player getSelectedPlayer(){
+		return selectedPlayer;
+	}
 
 	/**
 	 * Create the panel.
@@ -46,26 +62,22 @@ public class PlayerConfigUI extends JPanel {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setFont(new Font("Verdana", Font.BOLD, 8));
 		comboBox.setModel(new DefaultComboBoxModel(Difficulty.values()));
-		comboBox.setBounds(203, 290, 125, 22);
+		comboBox.setBounds(203, 290, 76, 22);
 		add(comboBox);
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				
-			}
-		});
-		btnNewButton.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Mechtron.png")));
-		buttonGroup_1.add(btnNewButton);
-		btnNewButton.setBackground(new Color(0, 0, 0));
-		btnNewButton.setActionCommand("");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton Mechtron = new JButton("");
+		Mechtron.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Mechtron.png")));
+		SelectRace.add(Mechtron);
+		Mechtron.setBackground(new Color(0, 0, 0));
+		Mechtron.setActionCommand("");
+		Mechtron.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				getSelectedPlayer().setRace(Race.MECHTRON);
+				System.out.println("Mechtron Selected");
 			}
 		});
-		btnNewButton.setBounds(30, 30, 76, 71);
-		add(btnNewButton);
+		Mechtron.setBounds(30, 30, 76, 71);
+		add(Mechtron);
 		
 		JToggleButton button = new JToggleButton("Color");
 		button.setFont(new Font("Verdana", Font.BOLD, 11));
@@ -75,93 +87,131 @@ public class PlayerConfigUI extends JPanel {
 		button.setBounds(116, 112, 76, 71);
 		add(button);
 		
-		JButton button_1 = new JButton("");
-		button_1.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Spheroid.png")));
-		buttonGroup_1.add(button_1);
-		button_1.setBackground(new Color(0, 0, 0));
-		button_1.setActionCommand("");
-		button_1.setBounds(202, 112, 76, 71);
-		add(button_1);
+		JButton Spheroid = new JButton("");
+		Spheroid.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Spheroid.png")));
+		SelectRace.add(Spheroid);
+		Spheroid.setBackground(new Color(0, 0, 0));
+		Spheroid.setActionCommand("");
+		Spheroid.setBounds(202, 112, 76, 71);
+		add(Spheroid);
 		
-		JButton button_2 = new JButton("");
-		button_2.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Bonzoid.png")));
-		buttonGroup_1.add(button_2);
-		button_2.setBackground(new Color(0, 0, 0));
-		button_2.setActionCommand("");
-		button_2.setBounds(30, 112, 76, 71);
-		add(button_2);
+		JButton Bonzoid = new JButton("");
+		Bonzoid.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Bonzoid.png")));
+		SelectRace.add(Bonzoid);
+		Bonzoid.setBackground(new Color(0, 0, 0));
+		Bonzoid.setActionCommand("");
+		Bonzoid.setBounds(30, 112, 76, 71);
+		add(Bonzoid);
 		
-		JButton button_3 = new JButton("");
-		button_3.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Gollumer.png")));
-		buttonGroup_1.add(button_3);
-		button_3.setBackground(new Color(0, 0, 0));
-		button_3.setActionCommand("");
-		button_3.setBounds(116, 30, 76, 71);
-		add(button_3);
+		JButton Gollumer = new JButton("");
+		Gollumer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getSelectedPlayer().setRace(Race.GOLLUMER);
+			}
+		});
+		Gollumer.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Gollumer.png")));
+		SelectRace.add(Gollumer);
+		Gollumer.setBackground(new Color(0, 0, 0));
+		Gollumer.setActionCommand("");
+		Gollumer.setBounds(116, 30, 76, 71);
+		add(Gollumer);
 		
-		JButton button_4 = new JButton("");
-		buttonGroup_1.add(button_4);
-		button_4.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Packer.png")));
-		button_4.setBackground(new Color(0, 0, 0));
-		button_4.setActionCommand("");
-		button_4.setBounds(202, 30, 76, 71);
-		add(button_4);
+		JButton Packer = new JButton("");
+		Packer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getSelectedPlayer().setRace(Race.PACKER);
+			}
+		});
+		SelectRace.add(Packer);
+		Packer.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Packer.png")));
+		Packer.setBackground(new Color(0, 0, 0));
+		Packer.setActionCommand("");
+		Packer.setBounds(202, 30, 76, 71);
+		add(Packer);
 		
-		JButton button_5 = new JButton("");
-		button_5.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Flapper.png")));
-		buttonGroup_1.add(button_5);
-		button_5.setBackground(new Color(0, 0, 0));
-		button_5.setActionCommand("");
-		button_5.setBounds(30, 194, 76, 71);
-		add(button_5);
+		JButton Flapper = new JButton("");
+		Flapper.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Flapper.png")));
+		SelectRace.add(Flapper);
+		Flapper.setBackground(new Color(0, 0, 0));
+		Flapper.setActionCommand("");
+		Flapper.setBounds(30, 194, 76, 71);
+		add(Flapper);
 		
-		JButton button_6 = new JButton("");
-		buttonGroup_1.add(button_6);
-		button_6.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Leggite.png")));
-		button_6.setBackground(new Color(0, 0, 0));
-		button_6.setActionCommand("");
-		button_6.setBounds(116, 194, 76, 71);
-		add(button_6);
+		JButton Leggite = new JButton("");
+		SelectRace.add(Leggite);
+		Leggite.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Leggite.png")));
+		Leggite.setBackground(new Color(0, 0, 0));
+		Leggite.setActionCommand("");
+		Leggite.setBounds(116, 194, 76, 71);
+		add(Leggite);
 		
-		JButton button_7 = new JButton("");
-		button_7.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Humanoid.png")));
-		buttonGroup_1.add(button_7);
-		button_7.setBackground(new Color(0, 0, 0));
-		button_7.setActionCommand("");
-		button_7.setBounds(202, 194, 76, 71);
-		add(button_7);
+		JButton Humanoid = new JButton("");
+		Humanoid.setIcon(new ImageIcon(PlayerConfigUI.class.getResource("/edu/gatech/spamr/resources/MULE_Humanoid.png")));
+		SelectRace.add(Humanoid);
+		Humanoid.setBackground(new Color(0, 0, 0));
+		Humanoid.setActionCommand("");
+		Humanoid.setBounds(202, 194, 76, 71);
+		add(Humanoid);
 		
-		JRadioButton radioButton = new JRadioButton("1");
-		radioButton.setForeground(Color.LIGHT_GRAY);
-		radioButton.setBackground(Color.DARK_GRAY);
-		radioButton.setFont(new Font("Verdana", Font.BOLD, 8));
-		buttonGroup.add(radioButton);
-		radioButton.setBounds(18, 298, 43, 23);
-		add(radioButton);
+		final JRadioButton Player1 = new JRadioButton("1");
+		Player1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(Player1.isEnabled()){
+					selectedPlayer = player1;
+			}
+		}});
+		Player1.setForeground(Color.LIGHT_GRAY);
+		Player1.setBackground(Color.DARK_GRAY);
+		Player1.setFont(new Font("Verdana", Font.BOLD, 8));
+		SelectPlayer.add(Player1);
+		Player1.setBounds(18, 298, 43, 23);
+		add(Player1);
 		
-		JRadioButton radioButton_1 = new JRadioButton("2");
-		radioButton_1.setForeground(Color.LIGHT_GRAY);
-		radioButton_1.setBackground(Color.DARK_GRAY);
-		radioButton_1.setFont(new Font("Verdana", Font.BOLD, 8));
-		buttonGroup.add(radioButton_1);
-		radioButton_1.setBounds(63, 298, 43, 23);
-		add(radioButton_1);
+		final JRadioButton Player2 = new JRadioButton("2");
+		Player2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(Player2.isEnabled()){
+					selectedPlayer = player2;
+				}
+			}
+		});
+		Player2.setForeground(Color.LIGHT_GRAY);
+		Player2.setBackground(Color.DARK_GRAY);
+		Player2.setFont(new Font("Verdana", Font.BOLD, 8));
+		SelectPlayer.add(Player2);
+		Player2.setBounds(63, 298, 43, 23);
+		add(Player2);
 		
-		JRadioButton radioButton_2 = new JRadioButton("3");
-		radioButton_2.setForeground(Color.LIGHT_GRAY);
-		radioButton_2.setBackground(Color.DARK_GRAY);
-		radioButton_2.setFont(new Font("Verdana", Font.BOLD, 8));
-		buttonGroup.add(radioButton_2);
-		radioButton_2.setBounds(105, 298, 43, 23);
-		add(radioButton_2);
+		final JRadioButton Player3 = new JRadioButton("3");
+		Player3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(Player3.isEnabled()){
+					selectedPlayer = player3;
+				}
+				System.out.println(selectedPlayer.getName());
+			}
+		});
+		Player3.setForeground(Color.LIGHT_GRAY);
+		Player3.setBackground(Color.DARK_GRAY);
+		Player3.setFont(new Font("Verdana", Font.BOLD, 8));
+		SelectPlayer.add(Player3);
+		Player3.setBounds(105, 298, 43, 23);
+		add(Player3);
 		
-		JRadioButton radioButton_3 = new JRadioButton("4");
-		radioButton_3.setForeground(Color.LIGHT_GRAY);
-		radioButton_3.setBackground(Color.DARK_GRAY);
-		radioButton_3.setFont(new Font("Verdana", Font.BOLD, 8));
-		buttonGroup.add(radioButton_3);
-		radioButton_3.setBounds(148, 298, 43, 23);
-		add(radioButton_3);
+		final JRadioButton Player4 = new JRadioButton("4");
+		Player4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(Player4.isEnabled()){
+					selectedPlayer = player4;
+				}
+			}
+		});
+		Player4.setForeground(Color.LIGHT_GRAY);
+		Player4.setBackground(Color.DARK_GRAY);
+		Player4.setFont(new Font("Verdana", Font.BOLD, 8));
+		SelectPlayer.add(Player4);
+		Player4.setBounds(148, 298, 43, 23);
+		add(Player4);
 		
 		JButton btnNewButton_1 = new JButton("Confirm");
 		btnNewButton_1.setFont(new Font("Verdana", Font.BOLD, 9));
@@ -187,6 +237,8 @@ public class PlayerConfigUI extends JPanel {
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 9));
 		lblNewLabel.setBounds(18, 277, 133, 22);
 		add(lblNewLabel);
+		
 
 	}
+	
 }
