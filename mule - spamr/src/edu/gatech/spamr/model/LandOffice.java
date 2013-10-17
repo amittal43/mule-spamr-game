4adab6f3c511a0b27e403927a12085c9286f47df
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-17 23:11:19 +0000

    Created LandOffice Class, method stubs

---
+++ mule - spamr/src/edu/gatech/spamr/model/LandOffice.java
@@ -0,0 +1,25 @@
+package edu.gatech.spamr.model;
+
+public class LandOffice {
+	
+	//TODO
+	public void buyLand(Player p, Tile t, int roundNum){
+		
+	}
+	
+	//TODO
+	public void sellLand(Player p, Tile t){
+		
+	}
+	
+	//TODO
+	public int calcBuyPrice(int roundNum){
+		return 0;
+	}
+	
+	//TODO
+	public int calcSellPrice(){
+		return 0;
+	}
+
+}
