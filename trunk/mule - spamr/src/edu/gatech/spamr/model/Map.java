59709af9171176675c82235d997986bece0f0171
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-17 23:11:40 +0000

    Added Tile Array

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Map.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Map.java
@@ -1,7 +1,11 @@
 package edu.gatech.spamr.model;
 
+
 public class Map {
 	private MapType mapType;
+	private Tile[] properties = new Tile[45];
+	
+	//TODO - Create Map ARRAY (45 TileType enums)
 	
 	public enum MapType{DEFAULT, OTHER};
 	
