6e77cd03cb7673cbf10dc3fa8d3d2cc47bcb4e79
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-17 22:39:38 +0000

    Updated Race and Difficulty enums with starting data

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Player.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Player.java
@@ -4,6 +4,7 @@ public class Player {
 
 	//data
 	private String name;
+	private int playerNum;
 	private Race race;
 	private Difficulty diff;
 	private PlayerColor color;
@@ -15,8 +16,38 @@ public class Player {
 	//array of Tile's which the player owns
 	
 	//enums
-	public enum Race{PACKER, SPHEROID, HUMANOID, LEGGITE, FLAPPER, BONZOID, MECHTRON, GOLLUMER};
-	public enum Difficulty{BEGINNER, STANDARD, TOURNAMENT};
+	public enum Race{PACKER(1000), SPHEROID(1000), HUMANOID(600), LEGGITE(1000), FLAPPER(1600), BONZOID(1000), MECHTRON(1000), GOLLUMER(1000);
+	private final int startingMoney;
+	
+	Race(int s){
+		startingMoney = s;
+	}
+	
+	public int getStartingMoney(){
+		return startingMoney;
+	}
+	
+	}
+	public enum Difficulty{BEGINNER(8,4), STANDARD(4,2), TOURNAMENT(4,2);
+	
+	private final int startingFood;
+	private final int startingEnergy;
+	
+	Difficulty(int food, int energy){
+		startingFood = food;
+		startingEnergy = energy;
+	}
+	
+	public int getStartingFood(){
+		return startingFood;
+	}
+	
+	public int getStartingEnergy(){
+		return startingEnergy;
+	}
+	
+	}
+	
 	public enum PlayerColor{BLUE, GREEN, RED, YELLOW};
 	
 	public Player(){
@@ -37,6 +68,14 @@ public class Player {
 	
 	public void setName(String n){
 		name = n;
+	}
+	
+	public int getPlayerNum(){
+		return playerNum;
+	}
+	
+	public void setPlayerNum(int num){
+		playerNum = num;
 	}
 	
 	public Race getRace(){
