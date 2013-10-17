f3f7e43155a406b26b539a8978d115486ec783aa
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-17 22:38:25 +0000

    Created Game class

---
+++ mule - spamr/src/edu/gatech/spamr/model/Game.java
@@ -0,0 +1,27 @@
+package edu.gatech.spamr.model;
+
+public class Game {
+
+	private int currentRound;
+	private int currentTurn;
+	private final int MAX_TURNS = 4;
+	
+	public int getCurrentRound(){
+		return currentRound;
+	}
+	
+	//TODO
+	public void updateRound(){
+		
+	}
+	
+	public int getCurrentTurn(){
+		return currentTurn;
+	}
+	
+	//TODO
+	public void updateTurn(){
+		
+	}
+	
+}
