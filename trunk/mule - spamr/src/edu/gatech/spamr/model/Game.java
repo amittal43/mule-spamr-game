5b89a013593948026e937fb49989aab796fd51b8
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-18 07:26:04 +0000

    combine updateTurn and updateRound into just an updateTurn() function that fixes both when called at the end of a player's turn

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Game.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Game.java
@@ -2,26 +2,29 @@ package edu.gatech.spamr.model;
 
 public class Game {
 
-	private int currentRound;
-	private int currentTurn;
+	private int currentRound = 1;	//game starts in round 1 
+	private int currentTurn = 1;	//game starts with the first player going
 	private final int MAX_TURNS = 4;
 	
-	public int getCurrentRound(){
-		return currentRound;
-	}
 	
-	//TODO
-	public void updateRound(){
-		
+	
+	//Should be called at the end of a player's turn once to update who's turn it is and what round the game is in
+	public void updateTurn(){
+		if (currentTurn == 4){
+			currentTurn = 1;
+			currentRound++;
+		} else {
+			currentTurn++;
+		}
 	}
 	
 	public int getCurrentTurn(){
 		return currentTurn;
 	}
 	
-	//TODO
-	public void updateTurn(){
-		
+	public int getCurrentRound(){
+		return currentRound;
 	}
+
 	
 }
