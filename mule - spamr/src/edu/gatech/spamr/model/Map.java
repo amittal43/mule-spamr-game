f8ba73449eaa2895ffd9078fa22567071004a350
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-18 07:19:30 +0000

    added the data for the default map

--- mule - spamr/src/edu/gatech/spamr/model/Map.java
+++ mule - spamr/src/edu/gatech/spamr/model/Map.java
@@ -1,11 +1,21 @@
 package edu.gatech.spamr.model;
 
+import java.util.ArrayList;
+
+import edu.gatech.spamr.model.Tile.TileType;
+
 
 public class Map {
 	private MapType mapType;
 	private Tile[] properties = new Tile[45];
 	
-	//TODO - Create Map ARRAY (45 TileType enums)
+	//Default Map Array for M.U.L.E.
+	private TileType[] defaultMap = new TileType[]{TileType.PLAIN, TileType.PLAIN, TileType.MOUNTAIN1, TileType.PLAIN, TileType.RIVER, TileType.PLAIN, TileType.MOUNTAIN3, TileType.PLAIN, TileType.PLAIN,
+			TileType.PLAIN, TileType.MOUNTAIN1, TileType.PLAIN, TileType.PLAIN, TileType.RIVER, TileType.PLAIN, TileType.PLAIN, TileType.PLAIN, TileType.MOUNTAIN3,
+			TileType.MOUNTAIN3, TileType.PLAIN, TileType.PLAIN, TileType.PLAIN, TileType.TOWN, TileType.PLAIN, TileType.PLAIN, TileType.PLAIN, TileType.MOUNTAIN1,
+			TileType.PLAIN, TileType.MOUNTAIN2, TileType.PLAIN, TileType.PLAIN, TileType.RIVER, TileType.PLAIN, TileType.MOUNTAIN2, TileType.PLAIN, TileType.PLAIN,
+			TileType.PLAIN, TileType.PLAIN, TileType.MOUNTAIN2, TileType.PLAIN, TileType.RIVER, TileType.PLAIN, TileType.PLAIN, TileType.PLAIN, TileType.MOUNTAIN2 };
+
 	
 	public enum MapType{DEFAULT, OTHER};
 	
