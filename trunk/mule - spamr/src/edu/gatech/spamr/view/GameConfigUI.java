d3aa1f3bd7f6f31f086661789c71b94a62e2dd1d
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-23 02:36:38 +0000

    condensed excessive code, reordered functions to be in a more logical order, commented code, no JavaDoc yet

--- trunk/mule - spamr/src/edu/gatech/spamr/view/GameConfigUI.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/view/GameConfigUI.java
@@ -44,104 +44,100 @@ import java.awt.Dimension;
 
 public class GameConfigUI extends JPanel {
 
-	private JPanel gameConfigPanel = this;
+	//GameConfigUI variables
 	private MainAppView parent;
 	private int numHumans = 4; // default number of human players
 	private MapType selectedMapType = MapType.DEFAULT;
-	
-	public MapType getMapType(){
-		return selectedMapType;
-	}
-	
-	public int getNumHumans(){
-		return numHumans;
-	}
-	
-	public void setParent(MainAppView mainApp){
-		parent = mainApp;
-	}
-	
-	private MainAppView getMainApp(){
-		return parent;
-	}
 	
 	/**
 	 * Create the panel.
 	 */
 	public GameConfigUI() {
+		//setting panel prefrences
 		setPreferredSize(new Dimension(309, 365));
 		setMinimumSize(new Dimension(309, 365));
 		setBackground(Color.DARK_GRAY);
 		setLayout(null);
 		
-		//combobox
-		final JComboBox MapComboBox = new JComboBox();
-		MapComboBox.setModel(new DefaultComboBoxModel(MapType.values()));
-		MapComboBox.setFont(new Font("Verdana", Font.BOLD, 11));
-		MapComboBox.setBounds(47, 113, 103, 20);
-		add(MapComboBox);
-		
-		final JComboBox HumansComboBox = new JComboBox();
-		HumansComboBox.setFont(new Font("Verdana", Font.BOLD, 11));
-		HumansComboBox.setModel(new DefaultComboBoxModel(new String[] {"4 Human Players", "3 Human Players", "2 Human Players"}));
-		HumansComboBox.setBounds(47, 180, 156, 20);
-		add(HumansComboBox);
-		
-		//label
-		JLabel lblMapSelection = new JLabel("Map Selection");
-		lblMapSelection.setForeground(Color.LIGHT_GRAY);
-		lblMapSelection.setFont(new Font("Verdana", Font.BOLD, 12));
-		lblMapSelection.setBounds(47, 88, 103, 14);
-		add(lblMapSelection);
-		
-		JLabel lblPlayerCount = new JLabel("Player Count");
-		lblPlayerCount.setForeground(Color.LIGHT_GRAY);
-		lblPlayerCount.setFont(new Font("Verdana", Font.BOLD, 12));
-		lblPlayerCount.setBounds(47, 155, 117, 14);
-		add(lblPlayerCount);
-		
+		//Title
 		JLabel lblGameConfiguration = new JLabel("Game Configuration");
 		lblGameConfiguration.setForeground(Color.LIGHT_GRAY);
 		lblGameConfiguration.setFont(new Font("Verdana", Font.BOLD, 18));
 		lblGameConfiguration.setBounds(47, 29, 208, 36);
 		add(lblGameConfiguration);
 		
-		//button
-		JButton btnContinue = new JButton("Continue");
-		btnContinue.setFont(new Font("Verdana", Font.BOLD, 12));
-		btnContinue.setBounds(102, 303, 103, 36);
-		add(btnContinue);
+		//Map combobox
+		final JComboBox MapComboBox = new JComboBox();
+		MapComboBox.setModel(new DefaultComboBoxModel(MapType.values()));
+		MapComboBox.setFont(new Font("Verdana", Font.BOLD, 11));
+		MapComboBox.setBounds(47, 113, 103, 20);
+		add(MapComboBox);
 		
+		//Human Count combobox
+		final JComboBox HumansComboBox = new JComboBox();
+		HumansComboBox.setFont(new Font("Verdana", Font.BOLD, 11));
+		HumansComboBox.setModel(new DefaultComboBoxModel(new String[] {"4 Human Players", "3 Human Players", "2 Human Players"}));
+		HumansComboBox.setBounds(47, 180, 156, 20);
+		add(HumansComboBox);
+		
+		//Map Label
+		JLabel lblMapSelection = new JLabel("Map Selection");
+		lblMapSelection.setForeground(Color.LIGHT_GRAY);
+		lblMapSelection.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblMapSelection.setBounds(47, 88, 103, 14);
+		add(lblMapSelection);
+		
+		//Player Count Label
+		JLabel lblPlayerCount = new JLabel("Player Count");
+		lblPlayerCount.setForeground(Color.LIGHT_GRAY);
+		lblPlayerCount.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblPlayerCount.setBounds(47, 155, 117, 14);
+		add(lblPlayerCount);
+		
+		//Difficulty combobox
 		final JComboBox DifficultyComboBox = new JComboBox();
 		DifficultyComboBox.setModel(new DefaultComboBoxModel(Difficulty.values()));
 		DifficultyComboBox.setFont(new Font("Verdana", Font.BOLD, 11));
 		DifficultyComboBox.setBounds(47, 246, 103, 22);
 		add(DifficultyComboBox);
 		
+		//Difficulty Label
 		JLabel lblDifficulty = new JLabel("Difficulty");
 		lblDifficulty.setForeground(Color.LIGHT_GRAY);
 		lblDifficulty.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblDifficulty.setBounds(47, 221, 103, 14);
 		add(lblDifficulty);
 		
-		//buttonListener
+		//Continue Button
+		JButton btnContinue = new JButton("Continue");
+		btnContinue.setFont(new Font("Verdana", Font.BOLD, 12));
+		btnContinue.setBounds(102, 303, 103, 36);
+		add(btnContinue);
+		
+		//buttonListener for Continue Button
 		btnContinue.addActionListener(new ActionListener() {  
             public void actionPerformed(ActionEvent e)
             {
-                //button is pressed
-            	selectedMapType = (MapType) MapComboBox.getSelectedItem();
+                //updates number of Human players based on the comboBox
             	if(HumansComboBox.getSelectedIndex()==0)
             		numHumans = 4;
             	if(HumansComboBox.getSelectedIndex()==1)
             		numHumans = 3;
             	if(HumansComboBox.getSelectedIndex()==2)
             		numHumans = 2;
+            	
+            	//updates Game with difficulty and map selected
             	Game.setDifficulty((Difficulty) DifficultyComboBox.getSelectedItem());
-            	Game.getMap().setMapType(selectedMapType);	//sets the map in game
+            	Game.getMap().setMapType((MapType) MapComboBox.getSelectedItem());
+            	
+            	//print debugging
+            	/*
             	System.out.println("Map Type: " + Game.getMap().getMapType().toString());
             	System.out.println("Number of Human Players: " + numHumans + " Players");
             	System.out.println("Difficulty: " + Game.getDifficulty().toString());
+            	*/
             	
+            	//closes pop up
             	parent.getConfigDialog().setContentPane(parent.getPlayerConfigUI());
             	parent.getConfigDialog().setTitle("Configure Players");
             	parent.getConfigDialog().validate();
@@ -152,4 +148,22 @@ public class GameConfigUI extends JPanel {
 		
 		
 	}
+	
+	//getters and setters
+	public MapType getMapType(){
+		return selectedMapType;
+	}
+	
+	public int getNumHumans(){
+		return numHumans;
+	}
+	
+	private MainAppView getMainApp(){
+		return parent;
+	}
+	
+	public void setParent(MainAppView mainApp){
+		parent = mainApp;
+	}
+	
 }
