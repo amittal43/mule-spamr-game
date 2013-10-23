ab8e7b46dd21f61e268647feacbc84566190f724
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-23 02:20:48 +0000

    commented not JavaDoc'd yet

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Game.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Game.java
@@ -4,17 +4,24 @@ import edu.gatech.spamr.model.Map.MapType;
 
 
 public class Game {
-
+	
+	//turn & round variables
 	private static int currentRound = 1;		//game starts in round 1 
 	private static int currentTurn = 1;			//game starts with the first player going
-	private final int MAX_TURNS = 4;	
+	private final int MAX_TURNS = 4;			//we assume a 4 player game so it has 4 turns per round
+	
+	//creates player object for each person
 	private static Player p1 = new Player();
 	private static Player p2 = new Player();
 	private static Player p3 = new Player();
 	private static Player p4 = new Player();
+	
+	//stores what map the game is going to use and the difficulty level
 	private static Map gameMap = new Map(MapType.DEFAULT);
 	private static Difficulty diff;
 	
+	//creates and describes the Difficulty enum
+	//each enum holds the starting food and energy for players in the game
 	public enum Difficulty{BEGINNER(8,4), STANDARD(4,2), TOURNAMENT(4,2);
 		
 		private final int startingFood;
@@ -44,7 +51,7 @@ public class Game {
 		}
 	}
 	
-	//getters and setters
+	//getters and setters 
 	public static int getCurrentTurn(){
 		return currentTurn;
 	}
@@ -53,6 +60,7 @@ public class Game {
 		return currentRound;
 	}
 	
+	//no setter for map as map class has a way to set type
 	public static Map getMap(){
 		return gameMap;
 	}
@@ -65,7 +73,7 @@ public class Game {
 		diff = d;
 	}
 	
-	//player gets
+	//player gets so their data can be updated by player class getters and setters
 	public static Player getPlayer1(){
 		return p1;
 	}
