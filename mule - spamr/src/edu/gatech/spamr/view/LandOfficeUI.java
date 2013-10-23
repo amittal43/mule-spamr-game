76a5911ef8dcc7906cfd3aee450eeb350a39be7e
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-23 01:16:59 +0000

    Added MapUI to LandOfficeUI

--- mule - spamr/src/edu/gatech/spamr/view/LandOfficeUI.java
+++ mule - spamr/src/edu/gatech/spamr/view/LandOfficeUI.java
@@ -1,14 +1,26 @@
 package edu.gatech.spamr.view;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class LandOfficeUI extends JPanel {
+	
+	private MapUI mapui = new MapUI();
 
 	/**
 	 * Create the panel.
 	 */
 	public LandOfficeUI() {
+		setPreferredSize(new Dimension(1280, 800));
+		setMinimumSize(new Dimension(1280, 800));
+		setBackground(Color.DARK_GRAY);
+		setLayout(null);
+		
+		JPanel panel = mapui;
+		mapui.setSize(new Dimension(800, 600));
+		panel.setBounds(45, 57, 820, 600);
+		add(panel);
 
 	}
-
 }
