2ec0580292af9d8cd34928d6bacb01cb610d488e
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-23 01:07:43 +0000

    added static map for game

--- mule - spamr/src/edu/gatech/spamr/model/Game.java
+++ mule - spamr/src/edu/gatech/spamr/model/Game.java
@@ -1,15 +1,17 @@
 package edu.gatech.spamr.model;
 
+import edu.gatech.spamr.model.Map.MapType;
+
 public class Game {
 
 	private static int currentRound = 1;		//game starts in round 1 
-	private static int currentTurn = 1;		//game starts with the first player going
+	private static int currentTurn = 1;			//game starts with the first player going
 	private final int MAX_TURNS = 4;	
 	private static Player p1 = new Player();
 	private static Player p2 = new Player();
 	private static Player p3 = new Player();
 	private static Player p4 = new Player();
-	
+	private static Map gameMap = new Map(MapType.DEFAULT);
 	
 	//Should be called at the end of a player's turn once to update who's turn it is and what round the game is in
 	public void updateTurn(){
