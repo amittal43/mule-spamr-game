7ea29977ac8996ba85345a7c1919365677d29fc1
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-25 02:51:44 +0000

    Completed calcGamble() method with correct formula

--- mule - spamr/src/edu/gatech/spamr/model/Pub.java
+++ mule - spamr/src/edu/gatech/spamr/model/Pub.java
@@ -1,9 +1,13 @@
 package edu.gatech.spamr.model;
 
+import java.util.Random;
+
 public class Pub {
 	
-	public void gamble(Player p){
-		
+	public int calcGamble(Round round, long timeLeft){
+		Random rand = new Random();
+		double secondsLeft = (double) timeLeft/1000;
+		return (int)(round.getGamblingBonus() + rand.nextInt((int)(2 * (2.14 * secondsLeft)+1))); // convert seconds to BTU then double
 	}
 
 }
