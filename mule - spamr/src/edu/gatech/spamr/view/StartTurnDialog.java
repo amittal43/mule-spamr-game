3b242a3a6134d64a8515cc8227fbc10c9d5b449c
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-10-31 21:22:24 +0000

    Created StartTurnDialog that will pop-up for the first player's turn

---
+++ mule - spamr/src/edu/gatech/spamr/view/StartTurnDialog.java
@@ -0,0 +1,76 @@
+package edu.gatech.spamr.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import edu.gatech.spamr.model.Game;
+import edu.gatech.spamr.model.Player;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.Dialog.ModalityType;
+
+public class StartTurnDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	JLabel lblPlayer = new JLabel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			StartTurnDialog dialog = new StartTurnDialog(Game.getCurrentPlayer());
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public StartTurnDialog(Player current) {
+		setMinimumSize(new Dimension(420, 260));
+		setSize(new Dimension(420, 260));
+		setModal(true);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setName("Player Turn");
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblCurrentPlayer = new JLabel("Current Player:");
+			lblCurrentPlayer.setFont(new Font("Verdana", Font.BOLD, 18));
+			lblCurrentPlayer.setBounds(123, 11, 198, 40);
+			contentPanel.add(lblCurrentPlayer);
+		}
+		
+		lblPlayer.setFont(new Font("Verdana", Font.BOLD, 18));
+		lblPlayer.setBounds(123, 56, 198, 47);
+		contentPanel.add(lblPlayer);
+		
+		JButton btnNewButton = new JButton("GO");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setFont(new Font("Verdana", Font.BOLD, 16));
+		btnNewButton.setBounds(168, 142, 101, 82);
+		contentPanel.add(btnNewButton);
+	}
+	
+	public void setCurrentPlayerText(String current){
+		lblPlayer.setText(current);
+	}
+}
