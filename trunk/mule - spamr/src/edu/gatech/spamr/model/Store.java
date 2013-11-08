73689b053885073b4affac67b67b7ea1b6042702
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-08 01:04:53 +0000

    Added sellResource method and finished buyMule method with respective fees

--- trunk/mule - spamr/src/edu/gatech/spamr/model/Store.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/Store.java
@@ -7,28 +7,75 @@ public class Store {
 	private int oreQuantity = Game.getDifficulty().getStoreOre();
 	private int muleQuantity = Game.getDifficulty().getStoreMULES();
 	
+	// starting prices
+	private int foodPrice = 30;
+	private int energyPrice = 25;
+	private int orePrice = 50;
+	private int mulePrice = 100; // base price
+	
+	// mule price addons
+	private final int foodFee = 25;
+	private final int energyFee = 50;
+	private final int oreFee = 75;
+	
 	public enum Resource{FOOD, ENERGY, ORE}
 	
 	public void buyResource(Player p, Resource r, int number){ // still need to implement money differences
 		if(r==Resource.FOOD){
 			p.setFood(p.getFood() + number);
 			foodQuantity -= number;
+			p.updateMoney(-(foodPrice * number));
 		}
 		
 		if(r==Resource.ENERGY){
 			p.setEnergy(p.getEnergy() + number);
 			energyQuantity -= number;
+			p.updateMoney(-(energyPrice * number));
 		}
 		
 		if(r==Resource.ORE){
 			p.setOre(p.getOre() + number);
 			oreQuantity -= number;
+			p.updateMoney(-(orePrice * number));
 		}
 	}
 	
-	public void buyMULE(Resource type, Player player, Tile tile){ // under construction
+	public void buyMULE(Resource type, Player player, Tile tile){
 		muleQuantity--;
-		//tile.setMule(type);
+		
+		if(type == Resource.FOOD){
+			player.updateMoney(-(mulePrice + foodFee));
+		}
+		
+		if(type == Resource.ENERGY){
+			player.updateMoney(-(mulePrice + energyFee));
+		}
+		
+		if(type == Resource.ORE){
+			player.updateMoney(-(mulePrice + oreFee));
+		}
+		
+		tile.setMule(type);
+	}
+	
+	public void sellResource(Player p, Resource r, int number){
+		if(r == Resource.FOOD){
+			p.setFood(p.getFood() - number);
+			foodQuantity += number;
+			p.updateMoney(foodPrice/2);
+		}
+		
+		if(r == Resource.ENERGY){
+			p.setEnergy(p.getEnergy() - number);
+			energyQuantity += number;
+			p.updateMoney(energyPrice/2);
+		}
+		
+		if(r == Resource.ORE){
+			p.setOre(p.getOre() - number);
+			oreQuantity += number;
+			p.updateMoney(orePrice/2);
+		}
 	}
 	
 }
