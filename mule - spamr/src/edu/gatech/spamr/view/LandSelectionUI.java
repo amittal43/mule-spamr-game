892b5dccc8df658bdf0540689249552e2a455d2c
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-08 01:11:41 +0000

    Print PlayerOrder for Debugging

--- mule - spamr/src/edu/gatech/spamr/view/LandSelectionUI.java
+++ mule - spamr/src/edu/gatech/spamr/view/LandSelectionUI.java
@@ -84,6 +84,12 @@ public class LandSelectionUI extends JPanel {
 				System.out.println("Land Selection Phase Over!");
 				parent.cardChangeTo("TurnScreen");
 				parent.setLandSelectionOver();
+				System.out.println("Player Order should be: "); // debugging
+				Player[] test = Game.getCurrentRound().calcTurn();
+				System.out.println("First: " + test[0].getName());
+				System.out.println("Second: " + test[1].getName());
+				System.out.println("Third: " + test[2].getName());
+				System.out.println("Last: " + test[3].getName());
 				Game.setPlayerOrder(Game.getCurrentRound().calcTurn());
 				//Game.decidePlayOrder(new Player[]{Game.getPlayer1(),Game.getPlayer2(), Game.getPlayer3(), Game.getPlayer4()});
 				parent.getTurnScreenUI().getTurnDialog().setCurrentPlayerText(Game.getCurrentPlayer().getName());
