bccbdbdca9d0048f77a4af7b1d1f7d8a8ff0ea1c
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-08 03:57:59 +0000

    Created MuleMenuUI to select Mule type and Tile to install on

---
+++ trunk/mule - spamr/src/edu/gatech/spamr/view/MuleMenuUI.java
@@ -0,0 +1,131 @@
+package edu.gatech.spamr.view;
+
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.JButton;
+
+import edu.gatech.spamr.model.Store.Resource;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.MatteBorder;
+
+public class MuleMenuUI extends JPanel {
+
+	private GameScreenUI parent;
+	private MapUI mapui = new MapUI();
+	
+	JButton btnFoodMule = new JButton("Food Mule");
+	JButton btnEnergyMule = new JButton("Energy Mule");
+	JButton btnOreMule = new JButton("Ore Mule");
+	JButton btnGoBack = new JButton("Go Back");
+	JButton btnConfirm = new JButton("Confirm");
+	
+	private JButton currentSelected = btnFoodMule;
+	private JButton lastSelected = btnConfirm;
+	private Resource selectedMule = Resource.FOOD;
+	
+	
+	/**
+	 * Create the panel.
+	 */
+	public MuleMenuUI() {
+		
+		setBackground(Color.DARK_GRAY);
+		setMinimumSize(new Dimension(1280, 800));
+		setPreferredSize(new Dimension(1280, 800));
+		mapui.setLocation(104, 108);
+		
+		mapui.setParent(parent);
+		mapui.updateBorders();
+		setLayout(null);
+		mapui.setBorder(null);
+		mapui.setMinimumSize(new Dimension(820, 600));
+		mapui.setSize(new Dimension(820, 600));
+		mapui.setPreferredSize(new Dimension(820, 600));
+		add(mapui);				//adds the panel
+		
+		JLabel playerTurnLabel = new JLabel("MULE Menu");
+		playerTurnLabel.setFont(new Font("Verdana", Font.BOLD, 25));
+		playerTurnLabel.setForeground(Color.LIGHT_GRAY);
+		playerTurnLabel.setBounds(104, 45, 163, 32);
+		add(playerTurnLabel);
+		btnFoodMule.setFont(new Font("Verdana", Font.BOLD, 16));
+		
+		btnFoodMule.setBorder(new LineBorder(Color.BLACK,2));
+		btnFoodMule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedMule = Resource.FOOD;
+				lastSelected = currentSelected;
+				lastSelected.setBorder(null);
+				currentSelected = btnFoodMule;
+				currentSelected.setBorder(new LineBorder(Color.BLACK,2));
+			}
+		});
+		btnFoodMule.setBounds(1000, 108, 170, 58);
+		add(btnFoodMule);
+		btnEnergyMule.setFont(new Font("Verdana", Font.BOLD, 16));
+		
+		btnEnergyMule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectedMule = Resource.ENERGY;
+				lastSelected = currentSelected;
+				lastSelected.setBorder(null);
+				currentSelected = btnEnergyMule;
+				currentSelected.setBorder(new LineBorder(Color.BLACK,2));
+			}
+		});
+		btnEnergyMule.setBounds(1000, 208, 170, 58);
+		add(btnEnergyMule);
+		btnOreMule.setFont(new Font("Verdana", Font.BOLD, 16));
+		
+		btnOreMule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedMule = Resource.ORE;
+				lastSelected = currentSelected;
+				lastSelected.setBorder(null);
+				currentSelected = btnOreMule;
+				currentSelected.setBorder(new LineBorder(Color.BLACK,2));
+			}
+		});
+		btnOreMule.setBounds(1000, 308, 170, 58);
+		add(btnOreMule);
+		btnGoBack.setFont(new Font("Verdana", Font.BOLD, 16));
+		btnGoBack.setPreferredSize(new Dimension(170, 58));
+		btnGoBack.setSize(new Dimension(170, 58));
+		
+		btnGoBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				parent.cardChangeTo("Store");
+			}
+		});
+		btnGoBack.setBounds(1000, 550, 170, 58);
+		add(btnGoBack);
+		btnConfirm.setSize(new Dimension(170, 58));
+		btnConfirm.setFont(new Font("Verdana", Font.BOLD, 16));
+		
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// buyMule() method
+				parent.cardChangeTo("Store");
+			}
+		});
+		btnConfirm.setBounds(1000, 650, 170, 58);
+		add(btnConfirm);
+
+	}
+
+
+	public void setParent(GameScreenUI gameScreenUI) {
+		parent = gameScreenUI;
+	}
+}
