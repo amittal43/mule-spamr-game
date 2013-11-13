704986a3abf11d090e612d643d37e345b7a468f9
puja.sheth17 <puja.sheth17@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-13 00:21:40 +0000

    javadoc the file

--- mule - spamr/src/edu/gatech/spamr/model/RandomEvent.java
+++ mule - spamr/src/edu/gatech/spamr/model/RandomEvent.java
@@ -2,6 +2,19 @@ package edu.gatech.spamr.model;
 
 import java.util.Random;
 
+/** 
+ * The RandomEvent class represents a class in which the list of events mandated
+ * on the T-Square site can be randomized and then used in either a good or bad 
+ * way to the player's resources.
+ *  
+ * @author Sean Lachenberg
+ * @author Aditya Mittal
+ * @author Mike Vail
+ * @author Puja Sheth
+ * @author Raj Prateek
+ * @version 1.0 11/12/2013 
+ */
+
 public class RandomEvent {
 	
 	Random random = new Random();
