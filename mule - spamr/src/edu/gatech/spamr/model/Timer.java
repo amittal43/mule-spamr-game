0dac9f09c9959ea0172c98b0bda5adf23339b820
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-14 23:12:34 +0000

    almost removed static, still broken, Game.updateTurn() is the only thing not fixed atm to get all models working

--- mule - spamr/src/edu/gatech/spamr/model/Timer.java
+++ mule - spamr/src/edu/gatech/spamr/model/Timer.java
@@ -4,24 +4,24 @@ import edu.gatech.spamr.view.GameScreenUI;
 
 public class Timer implements Runnable {
 	
-	private static GameScreenUI parent;
+	public  GameScreenUI parent;
 	
-	private volatile static boolean stop = false;
-	private static long turnTime = 0;
-	private static long turnEnd = 0;
-	private static long timeLeft = 0;
+	public volatile  boolean stop = false;
+	public  long turnTime = 0;
+	public  long turnEnd = 0;
+	public  long timeLeft = 0;
 
 	//Runnable method
-	public void run() {
+	public void run(Player currentPlayer, Round currentRound) {
         System.out.println("Timer is running!");
-        requestStart();
+        requestStart(parent);
         //the while loop
-        startTurn(Game.getCurrentPlayer(), Game.getCurrentRound());
+        startTurn(currentPlayer, currentRound);
     }
 	
 	
 	/////////methods/////////
-	public static long calcTurnTime(Player player, Round round){
+	public  long calcTurnTime(Player player, Round round){
 		if(player.getFood() >= round.getFoodRequirement()){ // no food shortage
 			System.out.println("The player has 50 seconds");
 			return 50000; //50.00 seconds
@@ -40,30 +40,30 @@ public class Timer implements Runnable {
 		return turnTime;
 	}
 	
-	public static void setTurnTime(Player player, Round round){
+	public  void setTurnTime(Player player, Round round){
 		turnTime = calcTurnTime(player,round);
 	}
 	
-	public static void setTurnEndTime(Player player, Round round){ // must setTurnTime before this is called
+	public  void setTurnEndTime(Player player, Round round){ // must setTurnTime before this is called
 		long start = System.currentTimeMillis();
 		turnEnd = start + turnTime;
 	}
 	
-	public static long getTimeRemaining(){
+	public  long getTimeRemaining(){
 		return turnEnd - System.currentTimeMillis();
 	}
 	
-	public static double getSecondsRemaining(){
+	public  double getSecondsRemaining(){
 		return (double)getTimeRemaining()/1000;
 	}
 	
-	public static void setTimeLeft(long time){
+	public  void setTimeLeft(long time){
 		timeLeft = time;
 	}
 	
 	
 	
-	public static void startTurn(Player player, Round round) {
+	public  void startTurn(Player player, Round round) {
 		setTurnTime(player,round);
 		setTurnEndTime(player,round);
 		while(System.currentTimeMillis()<turnEnd && !stop){
@@ -73,13 +73,15 @@ public class Timer implements Runnable {
 		setTimeLeft(getTimeRemaining());
 		if (timeLeft <= 0){
 			System.out.println("Turn is over with " + ((double)timeLeft/1000) + " seconds left");
+			
 			//updates turn
 			Game.updateTurn();
-			System.out.println("Turn changes to " + Game.getCurrentPlayer().getName());
+			
+			System.out.println("Turn changes to " + player.getName());
 			//screen change
 			parent.cardChangeTo("TurnScreen");
 			//pop-up
-			parent.getTurnScreenUI().getTurnDialog().setCurrentPlayerText(Game.getCurrentPlayer().getName());
+			parent.getTurnScreenUI().getTurnDialog().setCurrentPlayerText(player.getName());
 			parent.getTurnScreenUI().getTurnDialog().setVisible(true);
 			
 			requestStop();
@@ -88,18 +90,25 @@ public class Timer implements Runnable {
 		System.out.println("Turn is over with " + ((double)timeLeft/1000) + " seconds left");
 	}
 	
-	public static void requestStop() {
+	public  void requestStop() {
         stop = true;
         //System.out.println("Thread should have stopped");
     }
 	
-	public static void requestStart() {
+	public  void requestStart(GameScreenUI gameParent) {
 		stop = false;
 		turnTime = 0;
 		turnEnd = 0;
 		timeLeft = 0;
-		parent = Game.getParent();
+		parent = gameParent;
         //System.out.println("Thread should be reset");
     }
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
