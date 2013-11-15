b90fbee74fb0b2e82f4cb6c2cb74762aaf5a02d4
chitownchampion <chitownchampion@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-15 02:47:10 +0000

    Fixed Image Icon Null Pointers

--- trunk/mule - spamr/src/edu/gatech/spamr/view/TownScreenUI.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/view/TownScreenUI.java
@@ -61,7 +61,7 @@ public class TownScreenUI extends JPanel {
 		
 		
 		//store Button
-		ImageIcon storeIcon = new ImageIcon(TownScreenUI.class.getResource("/edu/gatech/spamr/resources/potter/STORE.JPG"));
+		ImageIcon storeIcon = new ImageIcon(TownScreenUI.class.getResource("/edu/gatech/spamr/resources/Store-final.JPG"));
 		JButton storeButton = new JButton(storeIcon);
 		storeButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
