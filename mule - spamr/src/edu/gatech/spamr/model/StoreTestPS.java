2e32432a06edfb179f038f93b7a37150b2171676
puja.sheth17 <puja.sheth17@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-20 03:05:54 +0000

    Edited the commented portion

--- mule - spamr/src/edu/gatech/spamr/model/StoreTestPS.java
+++ mule - spamr/src/edu/gatech/spamr/model/StoreTestPS.java
@@ -37,22 +37,53 @@ public class StoreTestPS {
 }
 
 
-//public void buyResource(Player p, Resource r, int number){ // still need to implement money differences
-//	if(r==Resource.FOOD){
-//		p.setFood(p.getFood() + number);
-//		foodQuantity -= number;
-//		p.updateMoney(-(foodPrice * number));
-//	}
+//package edu.gatech.spamr.model;
+//
+//import java.io.Serializable;
+//
+//public class Store implements Serializable {
 //	
-//	if(r==Resource.ENERGY){
-//		p.setEnergy(p.getEnergy() + number);
-//		energyQuantity -= number;
-//		p.updateMoney(-(energyPrice * number));
-//	}
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = -8447124918015466998L;
 //	
-//	if(r==Resource.ORE){
-//		p.setOre(p.getOre() + number);
-//		oreQuantity -= number;
-//		p.updateMoney(-(orePrice * number));
+//	//beginner Difficulty starting food
+//	private int foodQuantity = 16;
+//	private int energyQuantity = 16;
+//	private int oreQuantity = 0;
+//	private int muleQuantity = 25;
+//
+//	
+//	// starting prices
+//	private int foodPrice = 30;
+//	private int energyPrice = 25;
+//	private int orePrice = 50;
+//	private int mulePrice = 100; // base price
+//	
+//	// mule price addons
+//	private final int foodFee = 25;
+//	private final int energyFee = 50;
+//	private final int oreFee = 75;
+//	
+//	public enum Resource{FOOD, ENERGY, ORE}  ***THIS IS MY PROBLEM***
+//	
+//	public void buyResource(Player p, Resource r, int number){ // still need to implement money differences
+//		if(r==Resource.FOOD){
+//			p.setFood(p.getFood() + number);
+//			foodQuantity -= number;
+//			p.updateMoney(-(foodPrice * number));
+//		}
+//		
+//		if(r==Resource.ENERGY){
+//			p.setEnergy(p.getEnergy() + number);
+//			energyQuantity -= number;
+//			p.updateMoney(-(energyPrice * number));
+//		}
+//		
+//		if(r==Resource.ORE){
+//			p.setOre(p.getOre() + number);
+//			oreQuantity -= number;
+//			p.updateMoney(-(orePrice * number));
+//		}
 //	}
-//}
