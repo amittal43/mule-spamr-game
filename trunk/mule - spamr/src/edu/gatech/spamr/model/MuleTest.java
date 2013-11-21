81e02250fc0d421995aaf0b42225caa58a1162ea
puja.sheth17 <puja.sheth17@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-21 06:45:40 +0000

    Javadoc of the JUnit tests plus some of the other model/UI files as necessary based on the email Mike sent... didn't see to much of a difference... maybe I'm just tired :(

--- trunk/mule - spamr/src/edu/gatech/spamr/model/MuleTest.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/MuleTest.java
@@ -7,105 +7,113 @@ import org.junit.Test;
 import edu.gatech.spamr.model.Map.MapType;
 import edu.gatech.spamr.model.Store.Resource;
 
+/**
+ * MuleTest is a JUnit file to check if the buyMule method works.
+ * There are three different tests to check for 1. player money
+ * 2. tile type and 3. store quantity. 
+ * 
+ * @author Aditya Mittal
+ * @version 1.0 11/20/2013
+ */
+
 public class MuleTest {
 
 	@Test
 	public void testMoney() {
+		//initialize variables
 		Player p1 = new Player();
 		Player p2 = new Player();
 		Player p3 = new Player();
 		Player p4 = new Player();
 		
+		//set money
 		p1.setMoney(600);
 		p2.setMoney(500);
 		p3.setMoney(700);
 		p4.setMoney(1000);
-		
+
 		int money1 = p1.getMoney();
 		int money2 = p2.getMoney();
 		int money3 = p3.getMoney();
-		
-		
+
 		Store store = new Store();
 		store.getMuleQuantity();
 		store.getMulePrice(Resource.ENERGY);
 		store.getMulePrice(Resource.FOOD);
 		store.getMulePrice(Resource.ORE);
-		
+
 		Map map = new Map(MapType.DEFAULT);
 		Tile[] tileArray = map.getMapArray();
-		
-	
+
+		//create scenarios
 		store.buyMULE(Resource.ENERGY, p1, tileArray[10]);
-		assertEquals(p1.getMoney(), money1-(store.getMulePrice(Resource.ENERGY)));
-		
-		assertFalse(p1.getMoney() ==  money1-(store.getMulePrice(Resource.FOOD)));
-		
-		assertFalse(p1.getMoney() == money1-(store.getMulePrice(Resource.FOOD)));
-		
+		assertEquals(p1.getMoney(),
+				money1 - (store.getMulePrice(Resource.ENERGY)));
+
+		assertFalse(p1.getMoney() == money1
+				- (store.getMulePrice(Resource.FOOD)));
+
+		assertFalse(p1.getMoney() == money1
+				- (store.getMulePrice(Resource.FOOD)));
+
 		store.buyMULE(Resource.FOOD, p2, tileArray[12]);
-		assertEquals(p2.getMoney(), money2-(store.getMulePrice(Resource.FOOD)));
+		assertEquals(p2.getMoney(), money2
+				- (store.getMulePrice(Resource.FOOD)));
 		store.buyMULE(Resource.ORE, p3, tileArray[14]);
-		assertEquals(p3.getMoney(), money3 -(store.getMulePrice(Resource.ORE)));
-			
+		assertEquals(p3.getMoney(), money3 - (store.getMulePrice(Resource.ORE)));
+
 	}
+
 	@Test
-	public void tileTest(){
-		
+	public void tileTest() {
+		//initialize variables
 		Player p1 = new Player();
 		Player p2 = new Player();
 		Player p3 = new Player();
 
-		
 		Store store = new Store();
 		store.getMuleQuantity();
 		store.getMulePrice(Resource.ENERGY);
 		store.getMulePrice(Resource.FOOD);
 		store.getMulePrice(Resource.ORE);
-		
+
 		Map map = new Map(MapType.DEFAULT);
 		Tile[] tileArray = map.getMapArray();
 		
+		//test conditions
 		store.buyMULE(Resource.ENERGY, p1, tileArray[10]);
 		store.buyMULE(Resource.FOOD, p2, tileArray[12]);
 		store.buyMULE(Resource.ORE, p3, tileArray[35]);
-		
+
 		assertEquals(tileArray[10].getMule(), Resource.ENERGY);
 		assertEquals(tileArray[12].getMule(), Resource.FOOD);
 		assertEquals(tileArray[35].getMule(), Resource.ORE);
 		assertFalse(tileArray[12].getMule() == Resource.ENERGY);
-		
-		
+
 	}
-	
+
 	@Test
-	public void storeTest(){
+	public void storeTest() {
+		//initialize variables
 		Player p1 = new Player();
 		Player p2 = new Player();
 		Player p3 = new Player();
-		
+
 		Store store = new Store();
 		store.getMuleQuantity();
-		
+
 		int numMules = store.getMuleQuantity();
 
 		Map map = new Map(MapType.DEFAULT);
 		Tile[] tileArray = map.getMapArray();
 		
+		//test conditions
 		store.buyMULE(Resource.FOOD, p1, tileArray[10]);
 		store.buyMULE(Resource.ENERGY, p2, tileArray[15]);
 		store.buyMULE(Resource.ORE, p3, tileArray[20]);
-		
-		assertEquals(store.getMuleQuantity(), numMules-3); 
-		
+
+		assertEquals(store.getMuleQuantity(), numMules - 3);
 
 	}
 
 }
-
-
-
-
-
-
-
--- trunk/mule - spamr/src/edu/gatech/spamr/model/SellResourcesTest.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/SellResourcesTest.java
@@ -2,233 +2,228 @@ package edu.gatech.spamr.model;
 
 import static org.junit.Assert.*;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.junit.Before;
 import org.junit.Test;
 
 import edu.gatech.spamr.model.Store.Resource;
 
+/**
+ * SellResourcesTest is a JUnit file to check if the sellResource method works.
+ * There are different tests to check for 1. no food 2. selling some food
+ * 3. selling all food 4. no energy 5. some energy 6. all energy 7. no ore
+ * 8. some ore 9. all ore and 10. bad quantity. 
+ * 
+ * @author Mike Vail
+ * @version 1.0 11/20/2013
+ */
+
 public class SellResourcesTest {
 
-
 	@Test
-	public final void testSellNoFood(){
-		
-		//initialize scenario
+	public final void testSellNoFood() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setFood(0);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.FOOD;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results
-		assertEquals(0, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(0, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getFood());
 		assertEquals(16, testStore.getFoodQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellSomeFood(){
-		
-		//initialize scenario
+	public final void testSellSomeFood() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setFood(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.FOOD;
 		int testQuantity = 3;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(45, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(45, testPlayer.getMoney());
 		assertEquals(2, testPlayer.getFood());
 		assertEquals(19, testStore.getFoodQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellAllFood(){
-		
-		//initialize scenario
+	public final void testSellAllFood() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setFood(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.FOOD;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(75, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(75, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getFood());
 		assertEquals(21, testStore.getFoodQuantity());
-		
+
 	}
 
 	@Test
-	public final void testSellNoEnergy(){
-		
-		//initialize scenario
+	public final void testSellNoEnergy() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setEnergy(0);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ENERGY;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results
-		assertEquals(0, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(0, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getEnergy());
 		assertEquals(16, testStore.getEnergyQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellSomeEnergy(){
-		
-		//initialize scenario
+	public final void testSellSomeEnergy() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setEnergy(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ENERGY;
 		int testQuantity = 3;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(37, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(37, testPlayer.getMoney());
 		assertEquals(2, testPlayer.getEnergy());
 		assertEquals(19, testStore.getEnergyQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellAllEnergy(){
-		
-		//initialize scenario
+	public final void testSellAllEnergy() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setEnergy(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ENERGY;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(62, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(62, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getEnergy());
 		assertEquals(21, testStore.getEnergyQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellNoOre(){
-		
-		//initialize scenario
+	public final void testSellNoOre() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setOre(0);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ORE;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results
-		assertEquals(0, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(0, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getOre());
 		assertEquals(0, testStore.getOreQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellSomeOre(){
-		
-		//initialize scenario
+	public final void testSellSomeOre() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setOre(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ORE;
 		int testQuantity = 3;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(75, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(75, testPlayer.getMoney());
 		assertEquals(2, testPlayer.getOre());
 		assertEquals(3, testStore.getOreQuantity());
-		
+
 	}
-	
+
 	@Test
-	public final void testSellAllOre(){
-		
-		//initialize scenario
+	public final void testSellAllOre() {
+
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setOre(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ORE;
 		int testQuantity = 5;
-		
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(125, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(125, testPlayer.getMoney());
 		assertEquals(0, testPlayer.getOre());
 		assertEquals(5, testStore.getOreQuantity());
-		
+
 	}
-	
-	
+
 	@Test
-	public final void testBadQuantity(){
-		//initialize scenario
+	public final void testBadQuantity() {
+		// initialize scenario
 		Store testStore = new Store();
 		Player testPlayer = new Player();
 		testPlayer.setOre(5);
 		testPlayer.setMoney(0);
 		Resource testResource = Resource.ORE;
 		int testQuantity = -1;
-		
-		//run the function
+
+		// run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
-		
-		//check the results		
-		assertEquals(0, testPlayer.getMoney());		
+
+		// check the results
+		assertEquals(0, testPlayer.getMoney());
 		assertEquals(5, testPlayer.getOre());
 		assertEquals(0, testStore.getOreQuantity());
 	}
-	
+
 }
