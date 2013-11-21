bc1ccec44944aa842d20fdfe6c320ecc26316b35
twistedaxels <twistedaxels@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-21 04:06:28 +0000

    JUnit for SellReqources Complete

--- trunk/mule - spamr/src/edu/gatech/spamr/model/SellResourcesTest.java
+++ trunk/mule - spamr/src/edu/gatech/spamr/model/SellResourcesTest.java
@@ -12,147 +12,7 @@ import edu.gatech.spamr.model.Store.Resource;
 
 public class SellResourcesTest {
 
-	Store s1;
-	Player p1;
-	Player p2;
-	Player p3;
-	
-	/*@Before
-	public void setUp() {
-		
-		//initalizing the store
-		s1 = new Store();
-		
-		//players for certain cases
-		p1 = new Player();
-		p2 = new Player();
-		p3 = new Player();
-		
-		//no resources
-		p1.setFood(0);
-	    p1.setEnergy(0);
-	    p1.setOre(0);
-	    p1.setMoney(100);
-	    
-	    //single resources
-	    p2.setFood(1);
-	    p2.setEnergy(1);
-	    p2.setOre(1);
-	    p2.setMoney(100);
-	    
-	    //correct non singluar input
-	    p3.setFood(5);
-	    p3.setEnergy(5);
-	    p3.setOre(5);
-	    p3.setMoney(100);    
-	}*/
 
-	/*@Test
-	public final void testSellResource() {
-		
-		//player owns no resources and tries to sell them
-		//attempts to sell 5 Food when the player owns nothing
-		setUp();
-		s1.sellResource(p1, Resource.FOOD, 5);
-		
-		//attempts to sell 5 Energy when the player owns nothing
-		setUp();
-		s1.sellResource(p1, Resource.ENERGY, 5);
-		
-		//attempts to sell 5 Ore when the player owns nothing
-		setUp();
-		s1.sellResource(p1, Resource.ORE, 5);
-		
-		
-		//player owns a resource but sells more than they have
-		//attempts to sell 5 Food when the player owns 1
-		setUp();
-		s1.sellResource(p2, Resource.FOOD, 5);
-		
-		//attempts to sell 5 Energy when the player owns 1
-		setUp();
-		s1.sellResource(p2, Resource.ENERGY, 5);
-		
-		//attempts to sell 5 Ore when the player owns 1
-		setUp();
-		s1.sellResource(p2, Resource.ORE, 5);
-		
-		
-		//player sells the exact number of a resource that they have
-		//attempts to sell 5 Food when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.FOOD, 5);
-		
-		//attempts to sell 5 Energy when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ENERGY, 5);
-		
-		//attempts to sell 5 Ore when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ORE, 5);
-		
-		
-		//player sells less of a resource than they have
-		//attempts to sell 3 Food when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.FOOD, 3);
-		
-		//attempts to sell 3 Energy when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ENERGY, 3);
-		
-		//attempts to sell 3 Ore when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ORE, 3);
-		
-		
-		//player attempts to sell nothing
-		//attempts to sell 0 Food when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.FOOD, 0);
-		
-		//attempts to sell 0 Energy when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ENERGY, 0);
-		
-		//attempts to sell 0 Ore when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ORE, 0);
-		
-		
-		//player attempts to sell a single negative quantity of a resource
-		//attempts to sell -1 Food when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.FOOD, -1);
-		
-		//attempts to sell -1 Energy when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ENERGY, -1);
-		
-		//attempts to sell -1 Ore when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ORE, -1);
-		
-		
-		//player attempts to sell a negative amount of a resource
-		//attempts to sell -2 Food when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.FOOD, -2);
-		
-		//attempts to sell -2 Energy when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ENERGY, -2);
-		
-		//attempts to sell -2 Ore when the player owns 5
-		setUp();
-		s1.sellResource(p3, Resource.ORE, -2);
-
-		
-		
-		
-		fail("Not yet implemented"); // TODO
-	} */
-	
 	@Test
 	public final void testSellNoFood(){
 		
@@ -164,6 +24,7 @@ public class SellResourcesTest {
 		Resource testResource = Resource.FOOD;
 		int testQuantity = 5;
 		
+		
 		//run the function
 		testStore.sellResource(testPlayer, testResource, testQuantity);
 		
@@ -174,5 +35,200 @@ public class SellResourcesTest {
 		
 	}
 	
+	@Test
+	public final void testSellSomeFood(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setFood(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.FOOD;
+		int testQuantity = 3;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(45, testPlayer.getMoney());		
+		assertEquals(2, testPlayer.getFood());
+		assertEquals(19, testStore.getFoodQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellAllFood(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setFood(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.FOOD;
+		int testQuantity = 5;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(75, testPlayer.getMoney());		
+		assertEquals(0, testPlayer.getFood());
+		assertEquals(21, testStore.getFoodQuantity());
+		
+	}
 
+	@Test
+	public final void testSellNoEnergy(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setEnergy(0);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ENERGY;
+		int testQuantity = 5;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results
+		assertEquals(0, testPlayer.getMoney());		
+		assertEquals(0, testPlayer.getEnergy());
+		assertEquals(16, testStore.getEnergyQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellSomeEnergy(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setEnergy(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ENERGY;
+		int testQuantity = 3;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(37, testPlayer.getMoney());		
+		assertEquals(2, testPlayer.getEnergy());
+		assertEquals(19, testStore.getEnergyQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellAllEnergy(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setEnergy(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ENERGY;
+		int testQuantity = 5;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(62, testPlayer.getMoney());		
+		assertEquals(0, testPlayer.getEnergy());
+		assertEquals(21, testStore.getEnergyQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellNoOre(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setOre(0);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ORE;
+		int testQuantity = 5;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results
+		assertEquals(0, testPlayer.getMoney());		
+		assertEquals(0, testPlayer.getOre());
+		assertEquals(0, testStore.getOreQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellSomeOre(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setOre(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ORE;
+		int testQuantity = 3;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(75, testPlayer.getMoney());		
+		assertEquals(2, testPlayer.getOre());
+		assertEquals(3, testStore.getOreQuantity());
+		
+	}
+	
+	@Test
+	public final void testSellAllOre(){
+		
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setOre(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ORE;
+		int testQuantity = 5;
+		
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(125, testPlayer.getMoney());		
+		assertEquals(0, testPlayer.getOre());
+		assertEquals(5, testStore.getOreQuantity());
+		
+	}
+	
+	
+	@Test
+	public final void testBadQuantity(){
+		//initialize scenario
+		Store testStore = new Store();
+		Player testPlayer = new Player();
+		testPlayer.setOre(5);
+		testPlayer.setMoney(0);
+		Resource testResource = Resource.ORE;
+		int testQuantity = -1;
+		
+		//run the function
+		testStore.sellResource(testPlayer, testResource, testQuantity);
+		
+		//check the results		
+		assertEquals(0, testPlayer.getMoney());		
+		assertEquals(5, testPlayer.getOre());
+		assertEquals(0, testStore.getOreQuantity());
+	}
+	
 }
