fb3b687854aafbc79762b13175b551a4f94de13c
puja.sheth17 <puja.sheth17@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-25 07:24:12 +0000

    my attempt at changing the background

--- mule - spamr/src/edu/gatech/spamr/view/TitleScreenUI.java
+++ mule - spamr/src/edu/gatech/spamr/view/TitleScreenUI.java
@@ -1,15 +1,28 @@
 package edu.gatech.spamr.view;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+
 import java.awt.Color;
+
 import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Dimension;
+import java.awt.Image;
+
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+
 import javax.swing.SwingConstants;
+
 import java.awt.Component;
+import java.net.URL;
 
 /** 
  * The TitleScreenUI class creates a panel for the player to begin the game 
@@ -39,18 +52,24 @@ public class TitleScreenUI extends JPanel {
 		//sets preferences for the Title Screen
 		setPreferredSize(new Dimension(1280, 800));
 		setMinimumSize(new Dimension(1280, 800));
-		setBackground(Color.DARK_GRAY);
+		//import image
+		//URL url = new URL("/mule-spamr/src/edu/gatech/spamr/resources/potter/game screen.jpg");
+		//Image backgroundImage = ImageIO.read(game_screen.jpg);
+		
+		//set the background with image
+		JLabel background = new JLabel(new ImageIcon("/mule-spamr/src/edu/gatech/spamr/resources/potter/game_screen.jpg"));
+		add(background);
 		setLayout(null);
 		
 		//Title
-		JLabel lblNewLabel = new JLabel("MULE: SPAMR Edition");
-		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(479, 256, 359, 38);
-		lblNewLabel.setForeground(Color.LIGHT_GRAY);
-		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 30));
-		add(lblNewLabel);
+//		JLabel lblNewLabel = new JLabel("MULE: SPAMR Edition");
+//		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+//		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+//		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblNewLabel.setBounds(479, 256, 359, 38);
+//		lblNewLabel.setForeground(Color.LIGHT_GRAY);
+//		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 30));
+//		add(lblNewLabel);
 		
 		//start button
 		JButton btnStart = new JButton("START");
