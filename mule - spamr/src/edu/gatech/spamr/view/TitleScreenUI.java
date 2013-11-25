d265dd317bf05b92c06e371981f60909eb00a9e9
puja.sheth17 <puja.sheth17@5c929579-2f34-c34b-d60f-f44c03b3445a>
2013-11-25 07:35:12 +0000

    another attempt

--- mule - spamr/src/edu/gatech/spamr/view/TitleScreenUI.java
+++ mule - spamr/src/edu/gatech/spamr/view/TitleScreenUI.java
@@ -1,6 +1,5 @@
 package edu.gatech.spamr.view;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
@@ -8,21 +7,14 @@ import java.awt.Color;
 
 import javax.swing.JLabel;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Dimension;
-import java.awt.Image;
 
 import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.image.BufferedImage;
 
-import javax.swing.SwingConstants;
-
-import java.awt.Component;
-import java.net.URL;
 
 /** 
  * The TitleScreenUI class creates a panel for the player to begin the game 
@@ -52,14 +44,11 @@ public class TitleScreenUI extends JPanel {
 		//sets preferences for the Title Screen
 		setPreferredSize(new Dimension(1280, 800));
 		setMinimumSize(new Dimension(1280, 800));
-		//import image
-		//URL url = new URL("/mule-spamr/src/edu/gatech/spamr/resources/potter/game screen.jpg");
-		//Image backgroundImage = ImageIO.read(game_screen.jpg);
 		
 		//set the background with image
+		setLayout(null);
 		JLabel background = new JLabel(new ImageIcon("/mule-spamr/src/edu/gatech/spamr/resources/potter/game_screen.jpg"));
 		add(background);
-		setLayout(null);
 		
 		//Title
 //		JLabel lblNewLabel = new JLabel("MULE: SPAMR Edition");
